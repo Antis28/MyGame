4f1400f8d1f4a3ee8a1e6d527bd29bf1e1e1edd9
Antis <Antis28@mail.ru>
2022-09-03 15:23:40 +0300

    add точка входа для запуска

---
+++ ConsoleViewServer/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleViewServer
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            new MyGame.Sources.Main().Start();
+        }
+    }
+}
