655f21207cd911e6d2a490d27c5f778475c98a1c
Antis <Antis28@mail.ru>
2022-09-03 16:01:10 +0300

    add интерфейс и сам вывод сообщения(в консоль)

---
+++ MyGame/Sources/Services/ConsoleDebugLogService.cs
@@ -0,0 +1,14 @@
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using MyGame.Sources.Interfaces;
+
+namespace MyGame.Sources.Services
+{
+    public class ConsoleDebugLogService: ILogService
+    {
+        public void LogMessage(string message) {
+            System.Console.WriteLine(message);
+        }
+    }
+}
---
+++ MyGame/Sources/Services/JsonLogService.cs
@@ -0,0 +1,25 @@
+﻿// This is an independent project of an individual developer. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+using MyGame.Sources.Interfaces;
+
+namespace MyGame.Sources.Services
+{
+    public class JsonLogService : ILogService
+    {
+        string filepath;
+        string filename;
+
+        bool prettyPrint;
+
+        // etc...
+        public void LogMessage(string message)
+        {
+            // open file
+            // parse contents
+            // write new contents
+            // close file
+        }
+    }
+}
+
