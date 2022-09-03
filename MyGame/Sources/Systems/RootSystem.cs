bdaae70149f4a1dd458fc1db9cb7382e6675cd36
Antis <Antis28@mail.ru>
2022-09-03 15:25:40 +0300

    add создание и управление систем

---
+++ MyGame/Sources/Systems/RootSystem.cs
@@ -0,0 +1,19 @@
+﻿using MyGame.Sources.Systems;
+
+public sealed class RootSystem : Feature
+{
+    public RootSystem(Contexts contexts)
+    {
+        // Init
+        Add(new InitServerSystem(contexts));
+        // Input
+        
+        // Update
+
+        // Views / Render
+
+        // Events (Generated)
+
+        // Cleanup
+    }
+}
