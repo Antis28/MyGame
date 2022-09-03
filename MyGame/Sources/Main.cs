9696e9fedfc765977e77d12e76d99610f5dcbdf6
Antis <Antis28@mail.ru>
2022-09-03 15:21:15 +0300

    add core ECS
    имя решения и проекта должны сопадать для работы генератора кода Jenny???

---
+++ MyGame/Sources/Main.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+namespace MyGame.Sources
+{
+    public class Main
+    {
+        private RootSystem _systems;
+
+        public void Start()
+        {
+            var context = Contexts.sharedInstance;
+            _systems = new RootSystem(context);
+            _systems.Initialize();
+
+            while (true)
+            {
+                _systems.Execute(); 
+                Thread.Sleep(100);
+            }
+        }
+    }
+}
