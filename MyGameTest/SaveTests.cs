6a5dce62ad7051eb573df99c1170616cba8d1992
Antis <Antis28@mail.ru>
2022-10-02 19:42:31 +0300

    тест дял системы сохранения

---
+++ MyGameTest/SaveTests.cs
@@ -0,0 +1,25 @@
+﻿using MyGame.Sources.SaveLoad;
+using NSpec;
+using NUnit.Framework;
+
+namespace MyGameTest
+{
+    [TestFixture]
+    public class SaveTests : nspec
+    {
+        [Test]
+        public void WhenExecuting()
+        {
+            var expected = "Person.of.Interest.S04E08.720p.WEB.rus.LostFilm.TV";
+            var contexts = Contexts.sharedInstance;
+            var system = new SaveSystem(contexts);
+            var ent = contexts.game.CreateEntity();
+            ent.AddSettings(expected);
+
+            system.Execute();
+
+            Assert.True(ent.isDestroyed);
+            Assert.AreEqual(expected, ent.settings.lastFileName);
+        }
+    }
+}
