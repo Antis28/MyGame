eb248501a8dbe9a3d843201d254b6fd8ebf503e2
Antis <Antis28@mail.ru>
2022-10-02 19:39:27 +0300

    fix опечатку

--- MyGame/Sources/ServerCore/NotECS/ButtonClicker.cs
+++ MyGame/Sources/ServerCore/NotECS/ButtonClicker.cs
@@ -9,49 +9,80 @@ namespace MyGame.Sources.ServerCore
         {
             EmulateKeyDown(new MoveRight());
         }
+
         public static void MoveRight10Click()
         {
             EmulateKeyDown(new MoveRight10());
         }
+
         public static void MoveLeftClick()
         {
             EmulateKeyDown(new MoveLeft());
         }
+
         public static void MoveLeft10Click()
         {
             EmulateKeyDown(new MoveLeft10());
         }
+
         public static void MuteClick()
         {
             EmulateKeyDown(new Mute());
         }
+
         public static void PageDownClick()
         {
             EmulateKeyDown(new PageDown());
         }
+
         public static void PageUpClick()
         {
             EmulateKeyDown(new PageUp());
         }
+
         public static void PausePlayClick()
         {
             EmulateKeyDown(new PausePlay());
         }
+
         public static void VolumeDownClick()
         {
             EmulateKeyDown(new VolumeDown());
         }
+
         public static void VolumeUpClick()
         {
             EmulateKeyDown(new VolumeUp());
         }
+
         private static void EmulateKeyDown(KeyState state)
         {
             var procFinder = new ProcessFinder();
-            var hWnd = procFinder.GetActivePrcesses();
+            var hWnd = procFinder.GetActiveProcesses();
             KeyEmul emul = new KeyEmul();
 
             for (int i = 0; i < state.Repeat; i++) { emul.PostClick(hWnd, state.VKey); }
+        }
+
+        #region Nested Types
+
+        private interface KeyState
+        {
+            int Repeat { get; }
+            Int32 VKey { get; }
+        }
+
+        private struct MoveLeft : KeyState
+        {
+            public int VKey => (int)VirtualKeys.LEFT;
+
+            public int Repeat => 1;
+        }
+
+        private struct MoveLeft10 : KeyState
+        {
+            public int VKey => (int)VirtualKeys.LEFT;
+            public new int Repeat => 10;
         }
 
 
@@ -66,19 +97,6 @@ namespace MyGame.Sources.ServerCore
         {
             public int VKey => (Int32)VirtualKeys.RIGHT;
             public int Repeat => 10;
-        }
-
-        private struct MoveLeft : KeyState
-        {
-            public int VKey => (int)VirtualKeys.LEFT;
-
-            public int Repeat => 1;
-        }
-
-        private struct MoveLeft10 : KeyState
-        {
-            public int VKey => (int)VirtualKeys.LEFT;
-            public new int Repeat => 10;
         }
 
         private struct Mute : KeyState
@@ -123,10 +141,6 @@ namespace MyGame.Sources.ServerCore
             public int Repeat => 1;
         }
 
-        private interface KeyState
-        {
-            Int32 VKey { get; }
-            int Repeat { get; }
-        }
+        #endregion
     }
 }
