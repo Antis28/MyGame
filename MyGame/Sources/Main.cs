f3ebe5eab344b9a95c543cd268c9e79195f27c53
Antis <Antis28@mail.ru>
2022-11-14 00:09:05 +0300

    add минимальный клиент с запросом

--- MyGame/Sources/Main.cs
+++ MyGame/Sources/Main.cs
@@ -1,6 +1,9 @@
-﻿using System.Threading;
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
 using MyGame.Sources.Services;
 using MyGame.Sources.Systems;
+using CrossConsole;
 
 namespace MyGame.Sources
 {
@@ -19,6 +22,7 @@ namespace MyGame.Sources
             _systems.Initialize();
 
             context.game.CreateEntity().isLoadSettings = true;
+            Connect("hi");
 
             // для unity3D поместить в Update 
             while (true)
@@ -44,6 +48,43 @@ namespace MyGame.Sources
                 //     new UnityCameraService(),                  // camera bounds, zoom, fov, orthsize etc
                 //     new UnityPhysicsService()                  // raycast, checkcircle, checksphere etc.
             );
+        }
+        static async void  Connect(String message)
+        {
+            // Настраиваем его на IP нашего сервера и тот же порт.
+            String  server = "192.168.1.201";
+            Int32 port = 9090;
+            try
+            {
+                // Создаём TcpClient.
+                TcpClient client = new TcpClient();
+                await client.ConnectAsync(server, port);
+                
+                // Переводим наше сообщение в UTF8, а затем в массив Byte.
+                Byte[] data = System.Text.Encoding.UTF8.GetBytes(message);
+                // Получаем поток для чтения и записи данных.
+                NetworkStream stream = client.GetStream();
+                // Отправляем сообщение нашему серверу. 
+                await stream.WriteAsync(data, 0, data.Length);
+                ConsoleCreator.CreateForDotNetFramework().ShowMessage(message);
+                // Получаем ответ от сервера.
+                // Буфер для хранения принятого массива bytes.
+                data = new Byte[256];
+                // Строка для хранения полученных UTF8 данных.
+                // Читаем первый пакет ответа сервера. 
+                // Можно читать всё сообщение.
+                // Для этого надо организовать чтение в цикле как на сервере.
+                Int32 bytes = await stream.ReadAsync(data, 0, data.Length);
+                var responseData = System.Text.Encoding.UTF8.GetString(data, 0, bytes);
+                ConsoleCreator.CreateForDotNetFramework().ShowMessage(responseData);
+                
+                // Закрываем всё.
+                stream.Close();
+                client.Close();
+            } catch (ArgumentNullException e) { ConsoleCreator.CreateForDotNetFramework().ShowMessage(e.Message); } catch (SocketException e)
+            {
+                ConsoleCreator.CreateForDotNetFramework().ShowMessage(e.Message); 
+            }
         }
     }
 }
