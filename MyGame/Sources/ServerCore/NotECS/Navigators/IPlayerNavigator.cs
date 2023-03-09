cfe102527d386b721ab71635519cc8ceb785591d
Antis <Antis28@mail.ru>
2023-03-09 17:25:55 +0300

    format

--- MyGame/Sources/ServerCore/NotECS/Navigators/IPlayerNavigator.cs
+++ MyGame/Sources/ServerCore/NotECS/Navigators/IPlayerNavigator.cs
@@ -6,8 +6,10 @@ namespace MyGame.Sources.ServerCore;
 internal interface IPlayerNavigator
 {
     public void MoveRight();
+    public void MoveRight(int count);
     public void MoveRight10();
     public void MoveLeft();
+    public void MoveLeft(int count);
     public void MoveLeft10();
     public void Mute();
     public void Next();
