69a0ca69f9863b3ae466fdf544e1a213e4dc9897
Antis <antis28@mail.ru>
2023-09-28 15:25:46 +0300

    add muliThreading

--- MyGame/Sources/ClientProcessing/Systems/ReadClient.cs
+++ MyGame/Sources/ClientProcessing/Systems/ReadClient.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net;
 using System.Net.Sockets;
+using System.Threading;
 using Entitas;
 using MessageObjects;
 using MyGame.Sources.Debug;
@@ -13,9 +14,9 @@ namespace MyGame.Sources.ClientProcessing.Systems
     {
         private Contexts _contexts;
         private readonly IGroup<GameEntity> _entities;
+        private IPAddress ip;
+        private int port;
 
-        // Буфер для принимаемых данных.
-        readonly Byte[] bytes = new Byte[1024];
         String data = String.Empty;
 
 
@@ -29,31 +30,68 @@ namespace MyGame.Sources.ClientProcessing.Systems
         {
             try
             {
-                ClientQueryHandler();
+                foreach (var entity in _entities)
+                {
+                    TcpClient client = entity.client.value;
+
+                    Thread clientThread = new Thread(ClientQueryHandler);
+                    clientThread.Start(client);
+                    //ClientQueryHandler();
+                    entity.isDestroyed = true;
+                }
             }
             catch (Exception e)
             {
 
                 Main.Logger.ShowError(e);
             }
-            
+
         }
 
-        private void ClientQueryHandler()
+        private void ClientQueryHandler(object obj)
         {
-            foreach (var entity in _entities)
+            TcpClient client = (TcpClient)obj;
+
+            // получит ip клиента
+            var ipep = (IPEndPoint)client.Client.RemoteEndPoint;
+            ip = ipep.Address;
+            port = ipep.Port;
+
+            // Принимаем данные от клиента в цикле пока не дойдём до конца и отправит ответ об успехе.
+            try
             {
-                var client = entity.client.value;
-
-                // получит ip клиента
-                var ipep = (IPEndPoint)client.Client.RemoteEndPoint;
-                var ip = ipep.Address;
-                var port = ipep.Port;
-
-                // Принимаем данные от клиента в цикле пока не дойдём до конца и отправит ответ об успехе.
                 ReadAndSendSuccessAnswer(client);
+            }
+            catch (Exception e)
+            {
+                // TODO: Переделать в логер ECS(DI)
+                Main.Logger.ShowError(e);
+            }
+            finally
+            {
+                // Закрываем соединение.
+                client.Close(); 
+            }
+         }
 
-                entity.isDestroyed = true;
+        private void ReadAndSendSuccessAnswer(TcpClient client)
+        {
+            // Получаем информацию от клиента
+            var stream = client.GetStream();
+            // Буфер для принимаемых данных.
+            byte[] buffer = new byte[1024];
+
+            int bytesRead;
+            while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) != 0)
+            {
+                // Преобразуем данные в UTF8 string.
+                data = System.Text.Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                // Преобразуем полученную строку в массив Байт.
+                var msg = System.Text.Encoding.UTF8.GetBytes("Response: Success");
+
+                // Отправляем данные обратно клиенту (ответ).
+                stream.Write(msg, 0, msg.Length);
 
                 // Выводим в журнал полученное сообщение
                 DebugHelper.CreateEntityMessage(data, GetType().Name);
@@ -71,37 +109,7 @@ namespace MyGame.Sources.ClientProcessing.Systems
                     Main.Logger.ShowError(e);
                     return;
                 }
-
                 messageEntity.AddMessage(deserializedMessage, ip.ToString(), port);
-            }
-        }
-
-        private void ReadAndSendSuccessAnswer(TcpClient client)
-        {
-            try
-            {
-                // Получаем информацию от клиента
-                var stream = client.GetStream();
-                int count;
-                while ((count = stream.Read(bytes, 0, bytes.Length)) != 0)
-                {
-                    // Преобразуем данные в UTF8 string.
-                    data = System.Text.Encoding.UTF8.GetString(bytes, 0, count);
-
-                    // Преобразуем полученную строку в массив Байт.
-                    var msg = System.Text.Encoding.UTF8.GetBytes("Response: Success");
-
-                    // Отправляем данные обратно клиенту (ответ).
-                    stream.Write(msg, 0, msg.Length);
-                }
-            } catch (Exception e)
-            {
-                // TODO: Переделать в логер ECS(DI)
-                Main.Logger.ShowError(e);
-            } finally
-            {
-                // Закрываем соединение.
-                client.Close();
             }
         }
     }
